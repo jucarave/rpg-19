af9cd0dfb2f9d8624fffbe3441f58cc367b7346e
Camilo Ramirez <jucarave92@hotmail.com>
2019-04-15 00:17:12 -0500

    Added components support

---
+++ src/engine/Component.ts
@@ -0,0 +1,6 @@
+abstract class Component {
+    public start(): void {};
+    public update(): void {};
+}
+
+export default Component;
--- src/engine/Entity.ts
+++ src/engine/Entity.ts
@@ -2,10 +2,13 @@ import Vector2 from "./math/Vector2";
 import Sprite from "./geometries/Sprite";
 import Camera from "./Camera";
 import Matrix4 from "./math/Matrix4";
+import Component from "./Component";
 
 class Entity {
     private _sprite                     : Sprite;
     private _transMatrix                : Matrix4;
+    private _components                 : Array<Component>;
+    private _started                    : boolean;
 
     public readonly position            : Vector2;
 
@@ -13,12 +16,36 @@ class Entity {
         this.position = new Vector2(x, y);
         this._sprite = sprite;
         this._transMatrix = Matrix4.identity();
+        this._components = new Array();
+        this._started = false;
 
         this.position.onChange.add(() => { this._updateTransformationMatrix(); });
     }
 
     private _updateTransformationMatrix(): void {
         this._transMatrix.translate(this.position.x, this.position.y, 0.0);
+    }
+
+    public addComponent(component: Component): void {
+        this._components.push(component);
+
+        if (this._started) {
+            component.start();
+        }
+    }
+
+    public start(): void {
+        this._components.forEach((component: Component) => {
+            component.start();
+        });
+
+        this._started = true;
+    }
+
+    public update(): void {
+        this._components.forEach((component: Component) => {
+            component.update();
+        });
     }
 
     public render(camera: Camera): void {
